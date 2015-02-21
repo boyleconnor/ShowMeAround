0773b73d6e332050be82eeaa35453c483f040996
cascadianblue <cboyle@macalester.edu>
2015-02-20 18:39:39 -0600

    Adding models

--- Guides/models.py
+++ Guides/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.db.models import OneToOneField, ForeignKey, TextField, CharField, FloatField
+from django.db.models.base import Model
+from django.db.models.fields import BooleanField, IntegerField, DateTimeField
+from django.db.models.fields.files import ImageField
+from django.db.models.fields.related import ManyToManyField
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(Model):
+    is_guide = BooleanField(default=False)
+    user = OneToOneField(User)
+    profile_picture = ImageField()
+    name = TextField()
+
+    def get_rating(self):
+        return sum([float(review.stars) for review in self.reviews.all()]) / float(self.reviews.count())  # TODO: test this
+
+
+class Tour(Model):
+    title = TextField()
+    description = TextField()
+
+    guide = ForeignKey(Profile)
+    tourists = ManyToManyField(Profile)
+    capacity = IntegerField()
+
+    start_time = DateTimeField()
+    end_time = DateTimeField()
+
+    latitude = FloatField()
+    longitude = FloatField()
+
+    price = FloatField()
+
+
+class Review(Model):
+    title = CharField(max_length=255, blank=True)
+    text = TextField(blank=True)
+    author = ForeignKey(Profile)
+    subject = ForeignKey(Profile)
+    stars = IntegerField()
