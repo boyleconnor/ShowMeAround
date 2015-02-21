e8b45da3deeaec0ba3404637866319cffbcf70aa
cascadianblue <cboyle@macalester.edu>
2015-02-20 19:51:51 -0600

    Proper related_names added to models

--- Guides/models.py
+++ Guides/models.py
@@ -1,12 +1,9 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db.models import OneToOneField, ForeignKey, TextField, CharField, FloatField
 from django.db.models.base import Model
 from django.db.models.fields import BooleanField, IntegerField, DateTimeField
 from django.db.models.fields.files import ImageField
 from django.db.models.fields.related import ManyToManyField
-
-User = get_user_model()
 
 
 class Profile(Model):
@@ -23,8 +20,8 @@ class Tour(Model):
     title = TextField()
     description = TextField()
 
-    guide = ForeignKey(Profile)
-    tourists = ManyToManyField(Profile)
+    guide = ForeignKey(Profile, related_name='guided_tours')
+    tourists = ManyToManyField(Profile, related_name='taken_tours')
     capacity = IntegerField()
 
     start_time = DateTimeField()
@@ -39,6 +36,6 @@ class Tour(Model):
 class Review(Model):
     title = CharField(max_length=255, blank=True)
     text = TextField(blank=True)
-    author = ForeignKey(Profile)
+    author = ForeignKey(Profile, related_name='authored_reviews')
     subject = ForeignKey(Profile)
     stars = IntegerField()
