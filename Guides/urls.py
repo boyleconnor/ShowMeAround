f43a00ce72c22020066c4165afe731bbd35ed969
cascadianblue <cboyle@macalester.edu>
2015-02-21 00:49:05 -0600

    Added some urls

--- Guides/urls.py
+++ Guides/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from Guides.views import ProfileUpdate, ProfileDetail, TourList, TourCreate, leaveTour, joinTour
 
 urlpatterns = patterns('',
-    url(r'^profile/(?P<pk>\d+)/$', ProfileDetail.as_view()),
-    url(r'^profile/(?P<pk>\d+)/edit/$', ProfileUpdate.as_view(), name='profile.edit'),
+    url(r'^profile/(?P<pk>\d+)/$', ProfileDetail.as_view(), name='user.detail'),
+    url(r'^profile/(?P<pk>\d+)/edit/$', ProfileUpdate.as_view(), name='user.edit'),
     url(r'^$', TourList.as_view(), name='tour.list'),
     url(r'^tour/create/$', TourCreate.as_view(), name='tour.create'),
     url(r'^tour/leave/(\d+)/(\d+)', leaveTour, name='tour.leave'),
