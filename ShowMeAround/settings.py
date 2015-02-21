6e3066c88dc12eb73dc43c8b55e42312c1c077c9
Alan Morales <alanrmorales@gmail.com>
2015-02-20 19:56:28 -0600

    added template

--- ShowMeAround/settings.py
+++ ShowMeAround/settings.py
@@ -83,5 +83,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR,  'ShowMeAround/templates'),
 )
--- ShowMeAround/urls.py
+++ ShowMeAround/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name="tour/find.html")),
 )
