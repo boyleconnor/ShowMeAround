e2bc5b854d986637c79951fca2126fae57c05a10
Alan Morales <alanrmorales@gmail.com>
2015-02-21 09:24:56 -0600

    jfjfjfjf

--- Guides/migrations/0001_initial.py
+++ Guides/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 import django.utils.timezone
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Language',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('english_name', models.CharField(max_length=50)),
                 ('native_name', models.CharField(max_length=50)),
             ],
@@ -27,8 +27,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Review',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
-                ('title', models.CharField(max_length=255, blank=True)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('title', models.CharField(blank=True, max_length=255)),
                 ('text', models.TextField(blank=True)),
                 ('stars', models.IntegerField()),
             ],
@@ -39,7 +39,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Tour',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('title', models.TextField()),
                 ('description', models.TextField(help_text='On the description, please specify the general location(s) of the tour and ways to contact you. ')),
                 ('capacity', models.IntegerField()),
@@ -56,20 +56,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('last_login', models.DateTimeField(verbose_name='last login', default=django.utils.timezone.now)),
                 ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status', default=False)),
                 ('username', models.CharField(unique=True, max_length=30)),
                 ('email', models.EmailField(unique=True, max_length=75)),
-                ('name', models.CharField(max_length=100, blank=True)),
+                ('name', models.CharField(blank=True, max_length=100)),
+                ('profile_picture', models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Profile Pic')),
                 ('is_staff', models.BooleanField(default=False)),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_guide', models.BooleanField(default=False)),
                 ('date_joined', models.DateTimeField(auto_now_add=True)),
-                ('groups', models.ManyToManyField(help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', blank=True, related_name='user_set', verbose_name='groups', to='auth.Group', related_query_name='user')),
-                ('languages', models.ManyToManyField(null=True, to='Guides.Language', blank=True)),
-                ('user_permissions', models.ManyToManyField(help_text='Specific permissions for this user.', blank=True, related_name='user_set', verbose_name='user permissions', to='auth.Permission', related_query_name='user')),
+                ('groups', models.ManyToManyField(to='auth.Group', related_query_name='user', related_name='user_set', help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', blank=True, verbose_name='groups')),
+                ('languages', models.ManyToManyField(to='Guides.Language', blank=True, null=True)),
+                ('user_permissions', models.ManyToManyField(to='auth.Permission', related_query_name='user', related_name='user_set', help_text='Specific permissions for this user.', blank=True, verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
@@ -79,25 +80,25 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='tour',
             name='guide',
-            field=models.ForeignKey(related_name='guided_tours', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='guided_tours'),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='tour',
             name='language',
-            field=models.ForeignKey(null=True, blank=True, to='Guides.Language'),
+            field=models.ForeignKey(to='Guides.Language', null=True, blank=True),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='tour',
             name='tourists',
-            field=models.ManyToManyField(related_name='taken_tours', to=settings.AUTH_USER_MODEL, blank=True),
+            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name='taken_tours', blank=True),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='review',
             name='author',
-            field=models.ForeignKey(related_name='authored_reviews', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='authored_reviews'),
             preserve_default=True,
         ),
         migrations.AddField(
@@ -109,7 +110,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='review',
             name='tour',
-            field=models.ForeignKey(related_name='reviews', to='Guides.Tour'),
+            field=models.ForeignKey(to='Guides.Tour', related_name='reviews'),
             preserve_default=True,
         ),
         migrations.AlterUniqueTogether(
